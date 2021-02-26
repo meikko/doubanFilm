5de251da5321fac2d402e6d32a6f9dc5ad8e9c8e
房俊含 <853226786@qq.com>
2021-02-26 11:49:06 +0800

    数据获取

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // type:{
+    //   title:'',
+    //   list:[]
+    // }
+    types:[]
   },
 
   /**
@@ -19,7 +23,28 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    this.loadHotFilms();
 
+  },
+
+  loadHotFilms(){
+    wx.request({
+      url: 'https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items',
+      data:{
+        start:0,
+        count:6
+      },
+      success: res => {
+        console.log(res)
+        let type = {
+          title: res.data.subject_collection.name,
+          list:res.data.subject_collection_items
+        }
+        this.setData({
+          'types[0]':type
+        })
+      }
+    })
   },
 
   /**
